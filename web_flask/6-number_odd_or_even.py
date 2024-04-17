12b3c9d6a604b37d5c80855db215752c0d35e12b
AbaraEmmanuel <emmanuelabara265@gmail.com>
2024-04-17 16:33:35 +0100

    PEP

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,81 +1,56 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
-Start Flask app with root route
+Created on Tue Sep  1 14:42:23 2020
+@author: Robinson Montes
 """
-
-from flask import Flask, render_template
-
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """
-    Returns a string saying Hello HBNB
-    """
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web application"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Returns a string saying HBNB
-    """
-    return "HBNB"
+    """Adding a specific route /hbnb"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_with_text(text):
-    """
-    Displays "C", followed by value of <text>
-    """
-    tokens = text.split("_")
-    text = "C " + " ".join(tokens)
-    return text
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text=None):
+    """Dynamic inputed text: C + replace _ for space and show text"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_with_text(text):
-    """
-    Displays "Python", followed by value of <text>
-    """
-    tokens = text.split("_")
-    text = "Python " + " ".join(tokens)
-    return text
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Dynamic inputed text: Python + replace _ for space and show text"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def print_int(n):
-    """
-    Prints an integer if number passed is an int
-    """
-    if type(n) is int:
-        return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def only_digits_dynamic(n=None):
+    """Dynamic inputted integer"""
+    return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def print_int_template(n):
-    """
-    Renders special HTML page if number passed is an int
-    """
-    if type(n) is int:
-        return render_template("5-number.html", number=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def first_template(n=None):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_or_even(n):
-    """
-    Determines if number passed (if int) is odd or even
-    """
-    if type(n) is int:
-        odd = n % 2
-        if odd:
-            d = "odd"
-        else:
-            d = "even"
-        return render_template("6-number_odd_or_even.html", number=n, divisibility=d)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def conditional_templating(n=None):
+    """Display a HTML page only if n is an integer odd or even"""
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
